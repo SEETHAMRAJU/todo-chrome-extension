fe54357d2327d07b63d844d14b6d8c033d16236b
kingavatar <saikiranreddy06@gmail.com>
2021-04-01 20:42:07 +0530

    Added Browser Extension

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  pages: {
+    popup: {
+      template: "public/browser-extension.html",
+      entry: "./src/popup/main.ts",
+      title: "Popup",
+    },
+    options: {
+      template: "public/browser-extension.html",
+      entry: "./src/options/main.ts",
+      title: "Options",
+    },
+    standalone: {
+      template: "public/browser-extension.html",
+      entry: "./src/standalone/main.ts",
+      title: "Standalone",
+      filename: "index.html",
+    },
+  },
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: "src/background.ts",
+        },
+        contentScripts: {
+          entries: {
+            "content-script": ["src/content-scripts/content-script.ts"],
+          },
+        },
+      },
+    },
+  },
+};
